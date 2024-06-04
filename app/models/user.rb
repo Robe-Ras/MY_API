16443cc276687f1a3076af52bde59a7d0731488d
Robena Rasolond <82022919+Robe-Ras@users.noreply.github.com>
2024-06-04 23:33:55 +0200

    Initial commit

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  include Devise::JWT::RevocationStrategies::JTIMatcher
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :jwt_authenticatable, jwt_revocation_strategy: self
+
+  validates :email, presence: true, uniqueness: true
+
+  def jwt_payload
+    super
+  end
+end
---
+++ db/migrate/20240604170552_add_jti_to_users.rb
@@ -0,0 +1,16 @@
+class AddJtiToUsers < ActiveRecord::Migration[7.1]
+  def change
+    # Étape 1 : Ajouter la colonne sans la contrainte NOT NULL
+    add_column :users, :jti, :string
+
+    # Étape 2 : Mettre à jour les enregistrements existants
+    User.reset_column_information
+    User.find_each do |user|
+      user.update_columns(jti: SecureRandom.uuid)
+    end
+
+    # Étape 3 : Ajouter la contrainte NOT NULL et l'index unique
+    change_column_null :users, :jti, false
+    add_index :users, :jti, unique: true
+  end
+end
